361d5d00767b5772981a96bd80c04a517c201e41
Anna Voskresenskova <a.voskresenskova@atomyze.ru>
2022-12-22 21:44:15 +0300

    feature: homework

--- specs/app.spec.js
+++ specs/app.spec.js
@@ -1,8 +1,40 @@
 import { nameIsValid, fullTrim, getTotal } from '../src/app.js'
 
-/**
- * Для проверки, что jest настроен правильно. Можно удалить
- */
-test('adds 1 + 2 to equal 3', () => {
-    expect(1 + 2).toBe(3);
-});
+// describe ('nameIsValid', () => {
+//     it('checks name', () => {
+//         expect(nameIsValid('name')).toBe(true);
+//     })
+//     it('name not a number', () => {
+//         expect(nameIsValid(1)).toBe(false);
+//     })
+//     it('name length < 1 is not valid', () => {
+//         expect(nameIsValid('1')).toBe(false);
+//     })
+//     it('name does not include whitespace', () => {
+//         expect(nameIsValid('some name')).toBe(false);
+//     })
+// })
+
+// describe ('fullTrim', () =>{
+//     it('is function', () => {
+//         expect(typeof fullTrim).toBe('function')
+//     })
+//     it('whitespace is triming', () => {
+//         expect(fullTrim('omg its working')).toBe('omgitsworking')
+//     })
+//     it('return string', () => {
+//         expect(typeof fullTrim('some text')).toBe('string')
+//     })
+
+// })
+
+describe ('getTotal', () => {
+    test.each([
+        {items: [10, 10], expected: 100},
+        {items: [10, 1], expected: 10},
+        {items: [10, 1, 10, 9], expected: 100},
+        {items: [10, 1, 10, 9], expected: 100}
+     ])('getTotal($items)',({items, expected}) => {
+        expect(getTotal(items)).toBe(expected)
+     })
+})
